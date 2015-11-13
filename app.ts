0a9eb190e45e45c4754f499e92a44ad2ca1a2041
linsolas <linsolas@gmail.com>
2015-11-13 23:26:41 +0100

    fix: add timestamp

--- app.ts
+++ app.ts
@@ -9,14 +9,16 @@ import {HTTP_PROVIDERS, Http, Headers} from 'angular2/http';
     <h1>Ze Jarvis Chat</h1>
 
     <div class="row">
-      <div class="col-xs-8">
+      <div class="col-xs-9">
         <input class="form-control" type="text" #prompt>
       </div>
-      <div class="col-xs-4">
-        <button class="btn btn-primary" (click)="getMessages()">
-          Refresh <i class="fa fa-refresh"></i></button>
-        <button class="btn btn-primary" (click)="sendToJarvis(prompt.value)">
-          Send <i class="fa fa-arrow-down"></i></button>
+      <div class="col-xs-3">
+        <span class="pull-right">
+          <button class="btn btn-primary" (click)="getMessages()">
+            Refresh <i class="fa fa-refresh"></i></button>
+          <button class="btn btn-primary" (click)="sendToJarvis(username.value, prompt.value)">
+            Send <i class="fa fa-arrow-down"></i></button>
+        </span>
       </div>
     </div>
 
@@ -30,15 +32,22 @@ import {HTTP_PROVIDERS, Http, Headers} from 'angular2/http';
           </div>
           <div class="panel-body">
             <ul class="fa fa-ul">
-              <li *ng-for="#txt of jarvisMsg">
-                {{txt.user}} <i class="fa fa-angle-right"></i> {{txt.text}}               </li>
+              <li *ng-for="#msg of jarvisMsg">
+                <span class="timestamp">[{{msg.timestamp | date:'HH:mm:ss'}}]</span> <span class="user">{{msg.user}}</span> <i class="fa fa-angle-right"></i> <span class="message">{{msg.text}}</span></li>
             </ul>
           </div>
         </div>
       </div>
     </div>
-    <hr/>
-    {{jarvisMsg}}
+
+    <div class="row">
+      <div class="col-xs-2 col-xs-offset-6">
+        <span class="pull-right username">Your name:</span>
+      </div>
+      <div class="col-xs-4">
+        <input class="form-control" type="text" #username>
+      </div>
+    </div>
   </section>`
 })
 class AppComponent {
@@ -48,28 +57,29 @@ class AppComponent {
 
   constructor(@Inject(Http) http: Http) {
     this._http = http;
+    this.getMessages();
   }
 
-  sendToJarvis(msg: string): void {
+  sendToJarvis(username: string, msg: string): void {
     this._http.post('http://localhost:3000/messages',
       JSON.stringify({
         text: msg,
-        user: 'romain'
+        user: username ? username : 'guest'
       }), {
         headers: new Headers({
           'Content-Type': 'application/json'
         })
       })
       .subscribe((data) => {
-        this.jarvisMsg.push(JSON.parse(data._body));
+        this.jarvisMsg.unshift(JSON.parse(data._body));
       });
   }
 
   getMessages(): void {
+    console.log('get messages...');
     this._http.get('http://localhost:3000/messages')
       .map(res => res.json())
       .subscribe((msg: string[]) => {
-        console.log('not believable', msg);
         this.jarvisMsg = msg;
       });
   }
